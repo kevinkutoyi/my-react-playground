857e0209272dacc14f7d8ca2dbb1e5bc752bca70
kevinkutoyi <kevinkutoyi95@gmail.com>
2024-12-26 12:56:03 +0300

    importing a component file to another component file

---
+++ src/Components/Body.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Body = () => {
+    return (
+        <div>
+            {/* <h1>Welcome to My React Playground</h1> */}
+            <p>This is the body component.</p>
+            <p>This is the SECOND body component.</p>
+            <p>This is the THIRD body component.</p>
+        </div>
+    );
+};
+
+export default Body;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,13 @@
-import React from 'react';
+// import React from 'react';
+import Body from './Body';
 
 const Header = () => {
     return (
         <header>
             <h1>Welcome to My React Playground</h1>
+            <Body/>
         </header>
+        
     );
 };
 
